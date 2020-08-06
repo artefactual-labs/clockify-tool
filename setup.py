5662205719f9ddc22d402ef9228cc38d6bd81909
Mike Cantelon <mcantelon@gmail.com>
2020-08-06 01:50:18 -0700

    Added/changed files for packaging.

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Get requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Get text of the README file
+with open('README.md') as f:
+    README = f.read()
+
+
+setup(
+  name='clockifytool',
+  packages=['clockifytool'],
+  version='0.0.1',
+  license='MIT',
+  description='Tool to list, create, and delete time entries in Clockify',
+  author='Mike Cantelon',
+  author_email='mcantelon@gmail.com',
+
+  long_description=README,
+  long_description_content_type='text/markdown',
+
+  url='https://github.com/artefactual-labs/clockify-tool',
+
+  keywords=['clockify'],
+
+  install_requires=requirements,
+
+  scripts=['bin/cft'],
+)
